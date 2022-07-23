2d25d194d4a72d27cafff70649d5b4b48f811aa0
kaizheng-academic <74539350+kaizheng-academic@users.noreply.github.com>
2022-07-23 16:07:49 +0800

    Update GAPDA.py

--- easy-to-use/GAPDA.py
+++ easy-to-use/GAPDA.py
@@ -35,14 +35,16 @@ def GAPDA(isIndependent):
   
     N = X.shape[0]                # Number of nodes in the graph
     F = X.shape[1]                # Original feature dimension
+
+    
     n_classes = Y_train.shape[1]  # Number of classes
     F_ = 8                        # Output size of first GraphAttention layer
     n_attn_heads = 8              # Number of attention heads in first GAT layer
-    dropout_rate = 0.6            # Dropout rate (between and inside GAT layers)
-    l2_reg = 5e-4/2               # Factor for l2 regularization
-    learning_rate = 5e-3          # Learning rate for Adam
-    epochs = 10                # Number of training epochs
-    es_patience = 100             # Patience fot early stopping
+    dropout_rate = 0.02           # Dropout rate (between and inside GAT layers)
+    l2_reg = 5e-4/4               # Factor for l2 regularization
+    learning_rate = 5e-2          # Learning rate for Adam
+    epochs = 100                # Number of training epochs
+    es_patience = 150
 
     # Preprocessing operations
     X = preprocess_features(X)
